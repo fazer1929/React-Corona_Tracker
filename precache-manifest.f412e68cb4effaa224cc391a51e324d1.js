626251fb30283d36e903b7c35dbc467e76de2864
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-08-18 15:24:07 +0530

    Updates

---
+++ precache-manifest.f412e68cb4effaa224cc391a51e324d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cda7f0223ffda9ea2afdf88a3bdbed5",
+    "url": "/fazer1929/React-Corona_Tracker/index.html"
+  },
+  {
+    "revision": "48ca05de29e37cadb173",
+    "url": "/fazer1929/React-Corona_Tracker/static/css/main.3de5a2c2.chunk.css"
+  },
+  {
+    "revision": "78e2a378d9c63062f80a",
+    "url": "/fazer1929/React-Corona_Tracker/static/js/2.bfeee841.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/fazer1929/React-Corona_Tracker/static/js/2.bfeee841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ca05de29e37cadb173",
+    "url": "/fazer1929/React-Corona_Tracker/static/js/main.42083433.chunk.js"
+  },
+  {
+    "revision": "58b5b03ecb8cf108e8e9",
+    "url": "/fazer1929/React-Corona_Tracker/static/js/runtime-main.b59b1a1b.js"
+  },
+  {
+    "revision": "63dabe55bf05b250ddeeb0318117e38a",
+    "url": "/fazer1929/React-Corona_Tracker/static/media/logo.63dabe55.PNG"
+  }
+]);
