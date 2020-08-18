0f93f99ae8c8418e08830c281b82e71162750c76
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-08-18 17:29:25 +0530

    fixed some icon issues and dark mode

--- src/components/SwitchButton/SwitchButton.jsx
+++ src/components/SwitchButton/SwitchButton.jsx
@@ -5,9 +5,9 @@ import styles from './SwitchButton.module.css'
 const SwitchButton = ({changedarkmode,darktheme}) =>{
     return(
         <div className={styles.container}>
-            <Typography style={{display:'inline-block'}} variant="body1" component="p">Light</Typography>
+            <Typography style={{display:'inline-block',fontWeight:'700'}} variant="body1" component="p">Light</Typography>
             <Switch checked={darktheme} onChange={(a) => changedarkmode(a.target.checked)}></Switch>
-            <Typography variant="body1" style={{display:'inline-block'}} component="p">Dark</Typography>
+            <Typography variant="body1" style={{display:'inline-block',fontWeight:'700'}} component="p">Dark</Typography>
 
         </div>
     )
